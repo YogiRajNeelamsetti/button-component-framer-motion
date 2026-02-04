72b63c6f380fc07f50b1b50672ba94802ececa3f
NEELAMSETTI YOGIRAJ <yogineelamsetti@gmail.com>
2026-02-04 10:03:44 +0530

    feat: add 3D button component with Framer Motion animations

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import React from 'react';
+import { Content } from '../../components/Content';
+
+export default function Home() {
+  return (
+    <main>
+      <Content />
+    </main>
+  );
+}
